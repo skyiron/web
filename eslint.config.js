79d8b3e054c14b0d6ad66950b051b37bd3f0faef
Benedikt Kulmann <b.kulmann@opencloud.eu>
2025-02-17 16:14:24 +0100

    chore(ods): disable multi word component warning in ods docs

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,12 @@
 import openCloudConfig from '@opencloud-eu/eslint-config'
 
-export default [...openCloudConfig]
+export default [...openCloudConfig, {
+  "overrides": [
+    {
+      "files": ["packages/design-system/docs/**/*.vue"],
+      "rules": {
+        "vue/multi-word-component-names": "off"
+      }
+    }
+  ]
+}]
