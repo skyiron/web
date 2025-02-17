b6b837407e1f9ea7b4f6a5dbd989a10456b97ab6
Benedikt Kulmann <b.kulmann@opencloud.eu>
2025-02-17 16:13:52 +0100

    docs(ods): docs for text-input and textarea

---
+++ packages/design-system/docs/components/OcTextInput/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <oc-text-input v-model="name" label="Your name" />
+  <p>So your name is {{ name }}? Nice to meet you!</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const name = ref<string>('John Doe')
+</script>
