66cdaad93e58d6debb83f619418a382f849d14f1
Jannik Stehle <j.stehle@opencloud.eu>
2025-02-20 11:41:07 +0100

    fix: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,11 @@
 import openCloudConfig from '@opencloud-eu/eslint-config'
 
-export default [...openCloudConfig, {
-  "overrides": [
-    {
-      "files": ["packages/design-system/docs/**/*.vue"],
-      "rules": {
-        "vue/multi-word-component-names": "off"
-      }
+export default [
+  ...openCloudConfig,
+  {
+    files: ['packages/design-system/docs/**/*.vue'],
+    rules: {
+      'vue/multi-word-component-names': 'off'
     }
-  ]
-}]
+  }
+]
