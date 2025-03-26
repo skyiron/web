86cec1b9d090546fbb8ea71341a634322000a015
Jannik Stehle <j.stehle@opencloud.eu>
2025-03-26 10:05:57 +0100

    fix: remove invalid doc links
    Removes links to dev docs that are not working currently. They need to be re-added later.

--- packages/web-app-app-store/src/components/AppContextualHelper.vue
+++ packages/web-app-app-store/src/components/AppContextualHelper.vue
@@ -6,6 +6,5 @@
         'The App Store app lets you download apps as .zip files. Please follow the link below to learn how to install these apps into your OpenCloud.'
       )
     "
-    read-more-link="https://docs.opencloud.eu/services/web/#web-apps"
   />
 </template>
