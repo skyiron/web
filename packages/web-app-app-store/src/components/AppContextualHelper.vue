1f880f93a276dad9dcbf74f300fcee2a2910f493
Alex <abackermann91@gmail.com>
2025-04-14 09:22:10 +0200

    fix: introduce web applications link (#543)

--- packages/web-app-app-store/src/components/AppContextualHelper.vue
+++ packages/web-app-app-store/src/components/AppContextualHelper.vue
@@ -6,5 +6,6 @@
         'The App Store app lets you download apps as .zip files. Please follow the link below to learn how to install these apps into your OpenCloud.'
       )
     "
+    read-more-link="https://docs.opencloud.eu/docs/admin/configuration/web-applications"
   />
 </template>
