b4a339785958ef7e93580c465d51f389209c6ac8
Benedikt Kulmann <b.kulmann@opencloud.eu>
2025-05-08 16:22:22 +0200

    fix: remove duplicate font bundling

--- vite.config.ts
+++ vite.config.ts
@@ -229,7 +229,7 @@ export default defineConfig(({ mode, command }) => {
         viteStaticCopy({
           targets: (() => {
             const targets = [
-              ...['fonts', 'icons', 'images'].map((name) => ({
+              ...['icons', 'images'].map((name) => ({
                 src: `packages/design-system/src/assets/${name}/*`,
                 dest: `${name}`
               })),
